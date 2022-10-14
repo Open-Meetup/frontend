af90472fe38e06fee240e094309d793df90f6a40
Robert Casanova <robb.casanova@gmail.com>
2022-10-14 15:18:45 +0200

    chore: organize pages

---
+++ pages/create-meetup/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PageMeta from "~/content/components/PageMeta";
+import CreateMeetupContent from "~/content/pages/createMeetup";
+
+const CreateMeetup = () => (
+  <>
+    <PageMeta title="Create new meetup" />
+    <CreateMeetupContent />
+  </>
+);
+export default CreateMeetup;
